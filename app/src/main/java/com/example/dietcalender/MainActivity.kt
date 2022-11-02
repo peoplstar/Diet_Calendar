abaef2921db1371e4085e91c1c178a15e15c742b
Min gyu <78135526+peoplstar@users.noreply.github.com>
2022-11-02 20:57:41 +0900

    Complete Main Fragment

--- app/src/main/java/com/example/dietcalender/MainActivity.kt
+++ app/src/main/java/com/example/dietcalender/MainActivity.kt
@@ -13,5 +13,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        .add(R.id.frameLayout, mainView())
+        transaction.commit()
+
+        binding.mainBtn.setOnClickListener {
+            replaceFragment()
+        }
+    }
+    /*
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(fragment)
+        ...
+        그리고 사용할 프래그먼트만 show하고 나머지는 hide한다.
+
+        transaction.show(fragmentToUse)
+        transaction.hide(fragmentNotForUse)
+        ...
+        transaction.commit()
+     */
+    private fun replaceFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, mainView())
+        transaction.commit()
     }
 }
